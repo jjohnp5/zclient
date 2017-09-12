33c62e3c3cc72d8132e94c5d46bb88792dfe8c7a
John Lagmay <jjohnp5@gmail.com>
2017-09-12 12:00:34 -0500

    Updated connection options

--- mysql/connections.php
+++ mysql/connections.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect("localhost", "johnlagm_john", "USnavy1989", "johnlagm_users");
+$connect = mysqli_connect("localhost", "username", "passsword", "database");
 if(mysqli_connect_error()){
     die("Database connection failed.");
 }
